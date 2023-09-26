2f2e99a2b4e49b2a616372ffbf58dce150728436
yunxiang-li-github <yli4830@conestogac.on.ca>
2023-09-25 23:37:50 -0400

    test api using button

--- app/page.js
+++ app/page.js
@@ -1,36 +1,46 @@
+'use client';
+
 import axios from 'axios';
 
-export default async function Page() {
+const Page = () => {
+  const onSubmit = async (event) => {
+    event.preventDefault();
 
-  const url = 'https://top-ten-games.vercel.app/api/users/login';
-  const data = {
-    email: 'yunxiang.li.2023@gmail.com',
-    password: '123456',
-  };
+    const url = 'https://top-ten-games.vercel.app/api/users/login';
+    const data = {
+      email: 'yunxiang.li.2023@gmail.com',
+      password: '123456',
+    };
 
-  try {
-    const response = await axios.post(url, data, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+    try {
+      const response = await axios.post(url, data, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
 
-    // Check the response status code and handle it accordingly
-    if (response.status === 200) {
-      // Request was successful, you can handle the response here
-      console.log('Login successful:', response.data);
-    } else {
-      // Request failed, handle errors here
-      console.error('Login failed:', response.statusText);
+      // Check the response status code and handle it accordingly
+      if (response.status === 200) {
+        // Request was successful, you can handle the response here
+        console.log('Login successful:', response.data);
+      } else {
+        // Request failed, handle errors here
+        console.error('Login failed:', response.statusText);
+      }
+    } catch (error) {
+      // Handle any network or other errors here
+      console.error('An error occurred:', error);
     }
-  } catch (error) {
-    // Handle any network or other errors here
-    console.error('An error occurred:', error);
-  }
+  };
 
   return (
     <>
       <h1>Page</h1>
+      <form onSubmit={onSubmit}>
+        <button type='submit'>Call API</button>
+      </form>
     </>
   );
-}
+};
+
+export default Page;
